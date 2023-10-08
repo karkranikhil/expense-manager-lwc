56e9af8f3809f0625f51135c732cc880c7b2429c
nikhil <karkra.nikhil@gmail.com>
2023-10-08 18:51:56 +1100

    url update

--- src/modules/pages/login/login.js
+++ src/modules/pages/login/login.js
@@ -1,5 +1,5 @@
 import { LightningElement } from 'lwc';
-const SERVER_URL = "http://localhost:3002"
+const BACKEND_URL = "http://localhost:3002"
 export default class Login extends LightningElement{
     get loginUrl(){
         return `${BACKEND_URL}/oauth2/login`
