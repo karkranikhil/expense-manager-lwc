8052e0796f6593093e53c408c0164e0c43bfc9e8
nikhil <karkra.nikhil@gmail.com>
2023-10-08 15:18:48 +1100

    logout functionality

--- src/modules/components/navbar/navbar.js
+++ src/modules/components/navbar/navbar.js
@@ -1,9 +1,13 @@
 import { LightningElement, api } from "lwc";
-
+const SERVER_URL = "http://localhost:3002"
 export default class Navbar extends LightningElement{
     @api loggedInUser
 
     get userName(){
         return this.loggedInUser? this.loggedInUser.display_name :''
+    }
+
+    get logoutUrl(){
+        return `${SERVER_URL}/oauth2/logout`
     }
 }
